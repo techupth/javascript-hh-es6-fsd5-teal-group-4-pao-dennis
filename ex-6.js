1a6aae806e4096ebe7ee83b77430961ceae097d6
Pao <chaiwatt77@gmail.com>
2023-12-13 14:06:46 +0700

    ex1-7

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,8 @@
 let height = undefined;
 let result;
 
+function nlc(height){
+    return height??"Height is not defined";
+}
+result = nlc(height)
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
